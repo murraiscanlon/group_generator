cf475c44eeabfc2296b80296598814b041686387
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2021-01-03 18:21:40 -0600

    version1 of Student class

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,58 @@
+import java.util.ArrayList;
+
 public class Student {
+    String name;
+    String id;
+    String course;
+    String section;
+    ArrayList<String> groups = new ArrayList<>();
+
+
+    public Student(String name, String id, String course, String section) {
+        this.name = name;
+        this.id = id;
+        this.course = course;
+        this.section = section;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public ArrayList<String> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<String> groups) {
+        this.groups = groups;
+    }
 }
